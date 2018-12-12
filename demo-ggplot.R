bbfbfba44c48b59783c20295dbf39e0127cef9da
(U115136) <nicolas.stern@mobi.ch>
2018-12-12 17:36:39 +0100

    added one example

--- demo-ggplot.R
+++ demo-ggplot.R
@@ -8,8 +8,8 @@ library(ggplot2)
 x <- c(10, 20, 30, 40)
 y <- c(1000, 1500, 2200, 3800)
 df1 <- data.frame(x, y)
-n <- ggplot(df1, aes(df2$x, df2$y ))
-n +geom_bar(stat = "identity")
+n <- ggplot(df1, aes(df1$x, df1$y ))
+n + geom_bar(stat = "identity", col="red")
 
 # Simple Example 2
 ggplot(mtcars, aes(x=mtcars$cyl)) + geom_bar()
