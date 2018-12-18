bf7d029de90b819d8fe9bc46db86fe99b25e3911
(U115136) <nicolas.stern@mobi.ch>
2018-12-18 07:32:32 +0100

    added demo dplyr

--- demo-dplyr.R
+++ demo-dplyr.R
@@ -1,4 +1,9 @@
 # Erneut wird zuerst ein Subset für das Jahr 2009 erstellt
+
+library(gcookbook) # Für Daten
+library(GGally)
+help(countries)
+
 c2009<-countries %>%
   filter(Year==2009) %>%
   select(c(-Code,-Year))
